c60193310963410f675e6296b930b6ae00ac4c76
rizkinurfauzi <rizkinurfauzi0@gmail.com>
2020-10-22 14:29:14 +0700

    first commit

---
+++ Folder 1/pustaka_rizki/sampah/application/controllers/kenalin.php
@@ -0,0 +1,10 @@
+<?php
+class kenalin extends CI_Controller{
+    public function index() {
+        echo "<h1> perkenalan</h1>";
+        echo "Muhammad Hanif Aji Wiratama<br>
+                saya tinggal di Pagerbarang<br>
+                saya suka olahraga futsal";
+    }
+
+}
